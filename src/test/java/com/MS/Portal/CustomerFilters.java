e80a9380cc484f792b5d86a439a9736c8f067354
kranthibobba <kranthi.49@gmail.com>
2018-06-01 13:16:18 -0400

    UI_JAVA_TEST_FINAL

--- src/test/java/com/MS/Portal/CustomerFilters.java
+++ src/test/java/com/MS/Portal/CustomerFilters.java
@@ -1,32 +1,48 @@
 package com.MS.Portal;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
-
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class CustomerFilters extends Users {
 
+/*Scenario covers the following flow:
+ * User navigates to the User page
+ * Selects filter option from drop down menu
+ * Pass the input parameter
+ * validates the Row and Column values
+ */
+ 
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", driverPath+"chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.navigate().to("http://ec2-54-174-213-136.compute-1.amazonaws.com:8080/admin/users");
 		driver.manage().window().maximize();
+		Select username = new Select (driver.findElement(By.xpath(".//*[@id='q_username_input']/select")));
+		username.selectByIndex(0);
+		driver.findElement(By.xpath(".//*[@id='q_username']")).sendKeys("testkk");
+	    driver.findElement(By.xpath(".//*[@id='new_q']/div[4]/input[1]")).click();
+	    
+	    WebElement table = driver.findElement(By.xpath("//table[@class='index_table index']"));
+		List<WebElement> row = table.findElements(By.xpath("//table[@id='index_table_users']/tbody/tr"));
+		System.out.println("Total Number of Rows = " + row.size());
 		
+		List<WebElement> column = table.findElements(By.tagName("th"));
+		System.out.println("Total Number of Column = " + column.size());
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	//driver.close();	
+		for (int i=0;i<=row.size()-1 ;i++){
+			System.out.println(row.get(i).getText());
+		}
+						
+		List<WebElement> firstcol = driver.findElements(By.xpath("//table[@id='index_table_users']/tbody/tr/td[3]"));
+		for (int j=0; j<row.size();j++){
+			System.out.println(firstcol.get(j).getText());
+		}
+       driver.close();	
 	}
 
 }
--- src/test/java/com/MS/Portal/Users.java
+++ src/test/java/com/MS/Portal/Users.java
@@ -8,8 +8,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class Users {
+
+/*Scenario covers End-End Flow:
+ * Navigate to Users page,
+ * Check for existence & click on New User button, 
+ * Navigate to Signup page 
+ * Input invalid parameters to capture the error values
+ * Pass valid parameters and create customer account
+*/
 
 public static String driverPath = "C:/Users/e0215546/Downloads/chromedriver_win32/";
 public static WebDriver driver;
@@ -21,14 +30,13 @@ public void customersignup (String uname, String pwd, String eml){
 	        driver.findElement(By.name("user[email]")).sendKeys(eml);
 	        driver.findElement(By.name("commit")).click();       
 }
-
+@Test
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", driverPath+"chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get ("http://ec2-54-174-213-136.compute-1.amazonaws.com:8080/admin");
 		driver.manage().window().maximize();
-//Scenario1: Customer navigates to Users page and creates new account by selecting "newuser" option
 		driver.findElement(By.xpath("//*[@id='users']")).click();
 		String url = driver.getCurrentUrl();
 		if (url.equals("http://ec2-54-174-213-136.compute-1.amazonaws.com:8080/admin/users")){
@@ -57,12 +65,12 @@ public void customersignup (String uname, String pwd, String eml){
 			System.out.println("Customer couldn't navigate to the new user registration page");
 			}
 		Users passvalue = new Users();
-		//passvalue.customersignup("testkk","testss","testkk@gmail.com");
-		//String message = driver.findElement(By.xpath("//div[@class='flash flash_notice']")).getText();
-	    //System.out.println("Displayed msg is:"+message);
 	    passvalue.customersignup("","",""); 
 	    List<WebElement> errorredmsg = driver.findElements(By.xpath("//p[contains(@class, 'inline-errors')]"));
 	    System.out.println(errorredmsg.size());
-	    //driver.close();
+	    passvalue.customersignup("testkk","testss","testkk@gmail.com");
+	    String message = driver.findElement(By.xpath("//div[@class='flash flash_notice']")).getText();
+	  	System.out.println("Displayed msg is:"+message);
+	    driver.close();
 }
 }
